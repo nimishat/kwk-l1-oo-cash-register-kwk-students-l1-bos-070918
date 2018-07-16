7d36ff6fd401662b5717d3f60b65378fd4c00d4f
nimishat <nimishathakkar83@gmail.com>
2018-07-16 13:34:47 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -13,7 +13,7 @@ class AmazonTransaction
     end
    def apply_discount
      @total = total - ((@discount.to_f/100) * total)
-     puts "After the discount, the total comes to $#{@total}."
+     "After the discount, the total comes to $#{@total}."
    end
    end 
 
